956440132fdb0643f3a3eda7a9f090e1e208b447
ArisGoi <aris.goi97@gmail.com>
2021-09-06 13:46:06 +0200

    add generatore casuale

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // Un alert() espone 5 numeri generati casualmente.
+var winNumbers = [];
 
+for (i=0; i<5; i++){
+    winNumbers.push(Math.floor(Math.random() * 100 + 1));
+}
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
